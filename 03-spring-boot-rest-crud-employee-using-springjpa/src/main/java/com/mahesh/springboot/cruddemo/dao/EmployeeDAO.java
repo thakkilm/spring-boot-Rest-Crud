990d17dc6f77ba88263726949ea603ed5a0c7967
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-09 19:28:33 -0500

    Initiate project to work on spring data jpa

---
+++ 03-spring-boot-rest-crud-employee-using-springjpa/src/main/java/com/mahesh/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.mahesh.springboot.cruddemo.dao;
+
+import com.mahesh.springboot.cruddemo.entity.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+    Employee findByID(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+    void updateById(Employee employee);
+}
---
+++ 03-spring-boot-rest-crud-employee-using-springjpa/src/main/java/com/mahesh/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.mahesh.springboot.cruddemo.service;
+
+import com.mahesh.springboot.cruddemo.dao.EmployeeDAO;
+import com.mahesh.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findByID(int id) {
+        return employeeDAO.findByID(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateById(Employee employee) {
+        employeeDAO.updateById(employee);
+    }
+}
